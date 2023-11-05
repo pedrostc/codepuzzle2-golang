4f4899cd662c297e62be4f69a340d0444fba2033
Pedro Osternack Correa <pedrost.rj@gmail.com>
2023-11-05 18:25:04 -0500

    Formating tests

--- stringCalculator_test.go
+++ stringCalculator_test.go
@@ -3,35 +3,76 @@ package stringCalculator
 import "testing"
 
 func Test_Sum_EmptyString_Returns(t *testing.T) {
-	res, err := Sum("")
+	input := ""
 	expected := 0
 
-	if res != expected || err != nil {
-		t.Fatalf(`Sum("") = %d, %v, want match for %d, nil`, res, err, expected)
+	res, err := Sum(input)
+
+	if err != nil {
+		t.Fatalf(`Sum(%v) returned an error %v. Was expecting no error and the value %d.`, input, err, expected)
+	}
+
+	if res != expected {
+		t.Fatalf(`Sum(%v) returned %d. Was expecting %d.`, input, res, expected)
 	}
 }
 
 func Test_Sum_SingleValue_ReturnsParsedValue(t *testing.T) {
-	res, err := Sum("1")
+	input := "2"
 	var expected int = 2
 
-	if res != expected || err != nil {
-		t.Fatalf(`Sum("") = %d, %v, want match for %d, nil`, res, err, expected)
+	res, err := Sum(input)
+
+	if err != nil {
+		t.Fatalf(`Sum(%v) returned an error %v. Was expecting no error and the value %d.`, input, err, expected)
+	}
+
+	if res != expected {
+		t.Fatalf(`Sum(%v) returned %d. Was expecting %d.`, input, res, expected)
 	}
 }
 
 func Test_Sum_CSV_ReturnsSumOfValues(t *testing.T) {
-	res, err := Sum("1,3,4,2,5,7,18,100")
+	input := "1,3,4,2,5,7,18,100"
 	var expected int = 140
-	if res != expected || err != nil {
-		t.Fatalf(`Sum("") = %d, %v, want match for %d, nil`, res, err, expected)
+
+	res, err := Sum(input)
+
+	if err != nil {
+		t.Fatalf(`Sum(%v) returned an error %v. Was expecting no error and the value %d.`, input, err, expected)
+	}
+
+	if res != expected {
+		t.Fatalf(`Sum(%v) returned %d. Was expecting %d.`, input, res, expected)
 	}
 }
 
 func Test_Sum_NLSV_ReturnsSumOfValues(t *testing.T) {
-	res, err := Sum("1\n3\n4\n2")
-	var expected int = 10
-	if res != expected || err != nil {
-		t.Fatalf(`Sum("") = %d, %v, want match for %d, nil`, res, err, expected)
+	input := "1\n3\n4\n2"
+	expected := 10
+
+	res, err := Sum(input)
+
+	if err != nil {
+		t.Fatalf(`Sum(%v) returned an error %v. Was expecting no error and the value %d.`, input, err, expected)
+	}
+
+	if res != expected {
+		t.Fatalf(`Sum(%v) returned %d. Was expecting %d.`, input, res, expected)
+	}
+}
+
+func Test_Sum_NLSV_and_CSV_ReturnsSumOfValues(t *testing.T) {
+	input := "1\n3,4,2"
+	expected := 10
+
+	res, err := Sum(input)
+
+	if err != nil {
+		t.Fatalf(`Sum(%v) returned an error %v. Was expecting no error and the value %d.`, input, err, expected)
+	}
+
+	if res != expected {
+		t.Fatalf(`Sum(%v) returned %d. Was expecting %d.`, input, res, expected)
 	}
 }
